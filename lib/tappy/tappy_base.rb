b7a52b587cf5d71dc7a6783f423bb55dd4dd736d
siuying <siu.ying@gmail.com>
2010-06-10 23:42:39 +0800

    use original config var

--- lib/tappy/tappy_base.rb
+++ lib/tappy/tappy_base.rb
@@ -5,8 +5,8 @@ require 'json'
 
 module Tappy
   class TappyBase < Sinatra::Base
-    set :twitter_host, 'http://' + (ENV['APIGEE_TWITTER_ENDPOINT'] || "api.twitter.com")
-    set :twitter_search_host, 'http://' + (ENV['APIGEE_TWITTER_SEARCH_ENDPOINT'] || "search.twitter.com")
+    set :twitter_host, 'http://' + (ENV['APIGEE_TWITTER_API_ENDPOINT'] || "api.twitter.com")
+    set :twitter_search_host, 'http://' + (ENV['APIGEE_TWITTER_SEARCH_API_ENDPOINT'] || "search.twitter.com")
 
     set :filter, ENV["TAPPY_FILTER_CLASS"] || "Tappy::AgentFilter"
     set :filter_options, ENV["TAPPY_FILTER_OPTIONS"] || "foursquare"
